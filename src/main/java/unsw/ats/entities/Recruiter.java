2c60a24892cc62fa17152fbf1a43e7fb4dc46aa1
Steven You <yousilin@gmail.com>
2012-05-13 15:48:17 +1000

    entities

---
+++ src/main/java/unsw/ats/entities/Recruiter.java
@@ -0,0 +1,12 @@
+package unsw.ats.entities;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yousilin
+ * Date: 13/05/12
+ * Time: 3:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Recruiter {
+    private String userId;
+}
